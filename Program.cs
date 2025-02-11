af494bddb92e0ad167da7c5db909ba1be6ef0808
Rohan Drummond <drummond.rohan@gmail.com>
2025-02-11 15:06:11 +0700

    First commit!

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TipTapToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TipTapToe. Press Escape to exit. Let's get practicing!");
+            ConsoleKeyInfo keyInfo;
+            do 
+            {   
+                keyInfo = Console.ReadKey();
+                if (keyInfo.Key != ConsoleKey.Escape)
+                {
+                Console.WriteLine(keyInfo.Key);
+                }
+            }
+            while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
